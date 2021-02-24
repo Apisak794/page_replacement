ba8c119bf49fdf407134d7f433474e15e6be7a46
Apisak794 <apisak_vanitchakool@cmu.ac.th>
2021-02-24 07:58:17 +0700

    first clean code

--- Function.py
+++ Function.py
@@ -1,5 +1,3 @@
-import random
-
 def reformP(listp, num):
     old = 0
     for i in range(len(listp)):
@@ -110,40 +108,3 @@ def LRU(process, page):
     print('pageFaults of LRU algorithm = '+ str(pageFaults))
 
     
-    
-
-# process = [10, 3, 2, 14, 3, 6, 2, 1, 2, 12, 6, 1, 15, 15, 1, 0, 10, 15, 11, 0, 6, 4, 10, 4, 8, 
-# 11, 11, 14, 11, 11, 5, 2, 7, 13, 1, 9, 8, 4, 2, 8, 13, 15, 7, 14, 1, 9, 8, 9, 10, 1, 3, 15, 13, 14, 12, 0, 6, 4, 1, 1, 7, 7, 15, 5, 3, 11, 15, 4, 10, 3]
-# main
-# set input
-print('How many capacity of process : ')
-cap = int(input())
-print('How many frame : ')
-frame = int(input())
-
-# random process
-process = []
-for c in range(cap):
-    process.append(random.randrange(0, 10))
-print('')
-print('process =',process)
-
-# clear page before run FIFO algorithm
-page = []
-for i in range (frame):
-    page.append([str(i)])
-FIFO(process, page)
-
-# clear page before run OPTIMAL algorithm
-page = []
-for i in range (frame):
-    page.append([str(i)])
-Optiaml(process, page)
-
-# clear page before run LRU algorithm
-page = []
-for i in range (frame):
-    page.append([str(i)])
-LRU(process, page)
-
-
---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+import Function as Fn
+# set input
+print('How many capacity of process : ')
+cap = int(input())
+print('How many frame : ')
+frame = int(input())
+
+# random process
+process = []
+for c in range(cap):
+    process.append(random.randrange(0, 10))
+print('')
+print('process =',process)
+
+# clear page before run FIFO algorithm
+page = []
+for i in range (frame):
+    page.append([str(i)])
+Fn.FIFO(process, page)
+
+# clear page before run OPTIMAL algorithm
+page = []
+for i in range (frame):
+    page.append([str(i)])
+Fn.Optiaml(process, page)
+
+# clear page before run LRU algorithm
+page = []
+for i in range (frame):
+    page.append([str(i)])
+Fn.LRU(process, page)
